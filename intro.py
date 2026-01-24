98629b792a7c055a041a8fd6720710fd267536f3
Noha Shehab <nohashehab.iti@gmail.com>
2026-01-24 13:01:33 +0200

    introduction to python

---
+++ intro.py
@@ -0,0 +1,15 @@
+#
+#
+# print("Hello World")
+# name = 'Ahmed'
+# print(name)
+#
+# comment starts with #
+# you must start writing the script from
+# the beginning of the line
+# IndentationError: unexpected indent
+#     name = 'iti'
+
+"""
+you can use string without reference as a comment
+"""
